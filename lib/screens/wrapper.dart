231bec33b5db9bfb92c637bccbcf0f251d222fc4
Mateus Braun Vizotto <mateusbraunvizotto@gmail.com>
2020-06-25 14:41:34 -0300

    8 - Provider Package

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,10 +1,15 @@
 import 'package:brew_crew/screens/authenticate/authenticate.dart';
 import 'package:brew_crew/screens/home/home.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:brew_crew/models/user.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
+    print(user);
+
     // return either Home or Authenticate widget
     return Authenticate();
   }
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -11,9 +11,7 @@ class AuthService {
 
   // auth change user stream
   Stream<User> get user {
-    return _auth.onAuthStateChanged
-        //.map((FirebaseUser user) => _userFromFirebaseUser(user));
-        .map(_userFromFirebaseUser);
+    return _auth.onAuthStateChanged.map(_userFromFirebaseUser);
   }
 
   // sign in anon
