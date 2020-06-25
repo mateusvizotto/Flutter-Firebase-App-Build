a615f9b3b10a3f38cf7f8efab3d9b2bd2348b33c
Mateus Braun Vizotto <mateusbraunvizotto@gmail.com>
2020-06-25 16:15:37 -0300

    9 - Signing Out

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -31,5 +31,12 @@ class AuthService {
   // register with email & password
 
   // sign out
-
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
 }
