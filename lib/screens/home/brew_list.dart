f83b00646354436504bc7a212a28976c58ea1dbb
Mateus Braun Vizotto <mateusbraunvizotto@gmail.com>
2020-06-29 20:19:14 -0300

    19 - Brew Model

--- lib/screens/home/brew_list.dart
+++ lib/screens/home/brew_list.dart
@@ -1,5 +1,5 @@
+import 'package:brew_crew/models/brew.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
 
 class BrewList extends StatefulWidget {
@@ -10,11 +10,12 @@ class BrewList extends StatefulWidget {
 class _BrewListState extends State<BrewList> {
   @override
   Widget build(BuildContext context) {
-    final brews = Provider.of<QuerySnapshot>(context);
-    //print(brews.documents);
-    for (var doc in brews.documents) {
-      print(doc.data);
-    }
+    final brews = Provider.of<List<Brew>>(context);
+    brews.forEach((brew) {
+      print(brew.name);
+      print(brew.sugars);
+      print(brew.strength);
+    });
 
     return Container();
   }
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,7 +1,7 @@
+import 'package:brew_crew/models/brew.dart';
 import 'package:brew_crew/screens/home/brew_list.dart';
 import 'package:brew_crew/services/auth.dart';
 import 'package:brew_crew/services/database.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -10,7 +10,7 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<QuerySnapshot>.value(
+    return StreamProvider<List<Brew>>.value(
       value: DatabaseService().brews,
       child: Scaffold(
         backgroundColor: Colors.brown[50],
